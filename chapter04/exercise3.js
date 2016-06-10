9be89d7309f131b14d12cb8f9d98193f3d481adc
Elber Carneiro <elber@yieldmo.com>
2016-06-10 06:34:52 -0400

    changed launch.json to launch chapter04/exercise3.js

--- chapter04/exercise3.js
+++ chapter04/exercise3.js
@@ -1,16 +1,32 @@
 function arrayToList(array) {
-    
+  var list = null;
+  for (var i = array.length; i > 0; i--) {
+    var index = i - 1;
+    var value = array[index];
+    list = { value: value, rest: list };
   }
+
+  return list;
 }
 
-function listToArray() {
+function listToArray(list) {
+  var array = [];
+  while (list !== null) {
+    array.push(list.value);
+    list = list.rest;
+  }
+
+  return array;
+}
+
+function prepend(value, list) {
+  return { value: value, rest: list };
+}
+
+function nth(list, index) {
   
 }
 
-function prepend() {
-  
-}
-
-function nth() {
-  
-}
+console.log(arrayToList([3, 6, 9]));
+console.log(prepend(10, prepend(20, null)));
+console.log(listToArray(arrayToList([3, 6, 9])));
